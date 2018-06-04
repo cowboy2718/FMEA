498f64e743f509947ba1f9b51faf7359cd1fd9cf
Gojanovic <GY28@millercoors.com>
2018-06-04 09:20:13 -0600

    modify code

--- R/fmea.R
+++ R/fmea.R
@@ -2,23 +2,26 @@
 # Failure Mode Effects Analysis with R
 # June 2018
 
-#' Reading a FARS data set
-#' 
-#' @description
-#' This function reads in data set from the NHTSA Fatality Analysis Reporting System (FARS).
-#' @param filename FARS character filename in csv format to be read. If the filename is incorrect or not located, a message is issued.
-#' @return Returns a data frame from a csv file in tabular format.
-#' @note dplyr and tbl_df is used to format the returned data frame
-#' @importFrom dplyr tbl_df
-#' @importFrom readr read_csv
-#' @references  National Highway Traffic Safety Administration's Fatality Analysis Reporting System data set. https://www.nhtsa.gov/research-data/fatality-analysis-reporting-system-fars
+#' Data prep for a graphical depiction of risk based on an FMEA.
+#' @description  This function uses an imported data set and preps it for graphical analysis. Specifically, it adds a random jitter to severity, occurence and detection based on a standard normal variable (mean = 0 and sd = 0.25)
+#' @param filename FMEA filename as an imported data frame. 
+#' @importFrom dplyr mutate %>%
+#' @return Returns a modified data frame to be used in the graphical depiction of risk.
+#' @examples df<-prep_data(fmea_worksheet_name)
+#' @note Geom_jitter is by-passed with this function to allow ggrepel to work more efficiently.
 #' @export
 
-fmea <- function(filename) {
-  if(!file.exists(filename))
-    stop("file '", filename, "' does not exist")
-  data <- suppressMessages({
-    readr::read_csv(filename, progress = FALSE)
-  })
-  dplyr::tbl_df(data)
+# Create a dataframe for analysis and add a jitter component.
+
+prep_data<-function(filename){
+  rme_working<-filename                              # Calculates the number of rows in the FMEA worksheet
+  row_value<-nrow(rme_working)                       # Creates a row value variables                  
+  df.x<-as.data.frame(rnorm(row_value,0,0.25))       # Random jitter column
+  df.y<-as.data.frame(rnorm(row_value,0,0.25))       # Random jitter column
+  df.z<-as.data.frame(rnorm(row_value,0,0.25))       # Random jitter colums
+  df<-cbind(df.x,df.y,df.z)                          # Create a data frame with three random jitter columns
+  colnames(df)<-c("x","y","z")                       # Add column names
+  rme_working<-rme_working%>%mutate(sev=severity+df$x)%>%mutate(occ=occurrence+df$y)%>%mutate(det=detection+df$z) # Create new jitter variables
+  return(rme_working)                                # return the data set with new columns
 }
+
